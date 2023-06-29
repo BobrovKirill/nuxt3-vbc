1fa7021e92ae7a4246486a6dc50afb28a91e343d
BobrovKirill <bobrovkirill11@gmail.com>
2023-06-29 15:12:16 +0300

    start new project

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<h5 class="text-3xl font-bold underline text-blue-600">sdfsdf</h5>
+	</div>
+</template>
+
+<script setup>
+import '@/style.css';
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,41 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import eslint from 'vite-plugin-eslint';
+
+export default defineNuxtConfig({
+	app: {
+		head: {
+			charset: 'utf-16',
+			viewport: 'width=500, initial-scale=1',
+			htmlAttrs: {
+				lang: 'ru',
+			},
+			title: 'ВБЦ КириллБ',
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				// <meta name="description" content="My amazing site">
+				{ name: 'description', content: 'тестовое задание' },
+			],
+			script: [],
+			link: [],
+			style: [],
+			noscript: [],
+		},
+	},
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {
+				grid: 'autoplace',
+			},
+		},
+	},
+	css: ['~/style.css'],
+	modules: ['@nuxtjs/tailwindcss'],
+	imports: {
+		// autoImport: false
+	},
+	vite: {
+		plugins: [eslint()],
+	},
+});
